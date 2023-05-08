bee14db5696276ebd9b35cbb918ba9bba5c74537
gzmsrtkyozyldz <gzm.srtky@gmail.com>
2023-05-08 22:56:41 +0300

    make arithmetic operators

---
+++ arithmetic-operators.py
@@ -0,0 +1,11 @@
+number_1 = int(input("Please enter a number: "))
+number_2 = int(input("Please enter a number: "))
+
+addition = number_1 + number_2
+print(f'Result of addition: {addition}')
+subtraction = number_1 - number_2
+print(f'Result of subtraction: {subtraction}')
+multiplication = number_1 * number_2
+print(f'Result of multiplication: {multiplication}')
+division = number_1 / number_2
+print(f'Result of division: {division}')
