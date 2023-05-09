be11d543ec17d4de5d9ed984b2775be1b35a2688
gzmsrtkyozyldz <gzm.srtky@gmail.com>
2023-05-09 20:37:32 +0300

    add loops example

---
+++ loops.py
@@ -0,0 +1,9 @@
+# region loops
+"""
+Task
+The provided code stub reads and integer, n, from STDIN. For all non-negative integers i<n , print i*i.
+n = int(input('Please enter a number:'))
+for i in range(0, n):
+    print(i *i)
+"""
+# endregion
