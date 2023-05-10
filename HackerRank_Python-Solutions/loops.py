29ead2954b354455ad63e9b370d030eb4359f794
gzmsrtkyozyldz <gzm.srtky@gmail.com>
2023-05-10 13:02:58 +0300

    add hackerrank solutions

--- HackerRank_Python-Solutions/loops.py
+++ HackerRank_Python-Solutions/loops.py
@@ -1,9 +1,7 @@
 # region loops
-"""
-Task
-The provided code stub reads and integer, n, from STDIN. For all non-negative integers i<n , print i*i.
+#T ask
+# The provided code stub reads and integer, n, from STDIN. For all non-negative integers i<n , print i*i.
 n = int(input('Please enter a number:'))
 for i in range(0, n):
     print(i *i)
-"""
 # endregion
