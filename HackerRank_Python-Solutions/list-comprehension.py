50e415868774b37cbb025d5645adfc2afa94fb78
gzmsrtkyozyldz <gzm.srtky@gmail.com>
2023-05-10 12:32:32 +0300

    add hackerrank solutions

--- HackerRank_Python-Solutions/list-comprehension.py
+++ HackerRank_Python-Solutions/list-comprehension.py
@@ -1,10 +1,10 @@
 # region list-comprehension
-"""
+
 x = int(input('Please enter a number:'))
 y = int(input('Please enter a number:'))
 z = int(input('Please enter a number:'))
 n = int(input('Please enter a number:'))
 result = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
 print(result)
-"""
+
 # endregion
